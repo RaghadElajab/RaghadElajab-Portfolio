812810d4bf5e3fe39b4c230964d18ff46cbd8c77
RaghadElajab <raghad2015hussien@gmail.com>
2025-10-26 12:16:14 +0400

    Update and rename vite.config.js.txt to vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/raghad-portfolio/', // 👈 use your repo name here
+  plugins: [react()],
+})
